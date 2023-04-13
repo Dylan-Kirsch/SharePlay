dd0b593f337209a96714773caf4953c4b882b789
Dylan-Kirsch <118723863+Dylan-Kirsch@users.noreply.github.com>
2023-04-13 14:22:02 +0200

    profil

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,24 @@
+function toggleProfil() {
+    let profil = document.querySelector('.dropMenu');
+    let nav = document.querySelector('#navbar');
+    let nomSite = document.querySelector('#nomSite');
 
+    if (profil.style.display == "flex") 
+    {
+        profil.style.display = "none";
+        nav.style.backgroundColor = "#00000077";
+        nomSite.style.color = "#fff";
+        console.log('non visible');
+    }
+    else
+    {
+        profil.style.display = "flex";
+        nav.style.backgroundColor = "#fff";
+        nomSite.style.color = "#000";
+        console.log('visible');
+    }
+    
+}
 
+let btnProfil = document.querySelector('#btnProfil');
+btnProfil.addEventListener('click', toggleProfil);
