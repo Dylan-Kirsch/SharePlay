929cbbe7ad52c818bac1d6cc76dc4745522990fd
Dylan-Kirsch <kirsch-dylan@bbox.fr>
2023-04-18 10:05:55 +0200

    modif sql

--- univers.sql
+++ univers.sql
@@ -8,10 +8,10 @@ CREATE TABLE IF NOT EXISTS univers
 );
 
 INSERT INTO `jeux` (`id`, `titre`, `photo_default`) VALUES
-(1, 'Action' ,'atomic-heart.jpg'),
-(2, 'Action / Avebture' ,'battlefield-2042.jpeg'),
-(3, 'Course' ,'dead-island-2.jpg'),
-(4, 'Combat' ,'demon-souls-ps5.jpg'),
-(5, 'Jeux indée' ,'destiny-2-lightfall.jpeg'),
-(6, 'Survivol Horror' ,'elden-ring.jpg'),
-(7, 'Jeux de rôle' ,'final-fantasy-7.jpg'),
+(1, 'Action' ,'jeux-action.jpg'),
+(2, 'Aventure' ,'jeux-aventure.png'),
+(3, 'Course' ,'jeux-course.jpg'),
+(4, 'Combat' ,'jeux-combat.jpg'),
+(5, 'Jeux indée' ,'jeux-indee.jpg'),
+(6, 'Survivol Horror' ,'jeux-horror.jpg'),
+(7, 'Jeux de rôle' ,'jeux-de-role.png'),
